2631501e5785e570cee1ee82f63c76c6f501a60b
Sdoerty <roma-first@mail.ru>
2021-12-18 19:59:49 +0300

    add template search textField

---
+++ lib/pages/films_page/Films.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Movie{
+  final String filmImage;
+  final String filmName;
+  final String filmDate;
+  final String filmDescription;
+
+  Movie(this.filmImage, this.filmName, this.filmDate, this.filmDescription);
+}
